19e7957a942389afb93b6b173f6a0f8034362456
ANJUM FERDOUS <anjumferdous110@gmail.com>
2024-11-23 12:47:52 +0600

    Change the home page to the playground page

---
+++ alpha-clash.js
@@ -0,0 +1,19 @@
+
+function play() {
+
+  // const homeScreen = document.getElementById('home-screen');
+  // homeScreen.classList.add('hidden');
+
+  // const playGroundSection = document.getElementById("play-ground");
+  // playGroundSection.classList.remove('hidden');
+  hideElementById("home-screen");
+  showElementById("play-ground");
+
+}
+
+
+
+
+
+    
+  
