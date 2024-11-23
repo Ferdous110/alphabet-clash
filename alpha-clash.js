5e18ec61030f035a9d7913789120e2782a90420d
ANJUM FERDOUS <anjumferdous110@gmail.com>
2024-11-23 16:45:42 +0600

    Score update press the right key

--- alpha-clash.js
+++ alpha-clash.js
@@ -1,4 +1,33 @@
+function handleKeyboardButtonPress(event) {
+  const playerPressed = event.key;
 
+  // get the expected to press
+  const currentAlphabetElement = document.getElementById("current-alphabet");
+  const curentAlphabet = currentAlphabetElement.innerText;p
+  const expectedAlphabet = curentAlphabet.toLowerCase();
+
+  // check match or not
+
+  if (playerPressed === expectedAlphabet) {
+    console.log("you get a point");
+    const currentScoreElemnet = document.getElementById("current-score");
+    const currentScoreText = currentScoreElemnet.innerText;
+    const currentScore = parseInt(currentScoreText);
+    const newScore = currentScore + 1;
+
+    // show the update score 
+    currentScoreElemnet.innerText = newScore;
+
+    // start a new round 
+    removeBackgroundColorById(expectedAlphabet);
+    continueGame();
+
+  } else {
+    console.log("you missed. you lost a life");
+  }
+}
+// captupe keyboard key press
+document.addEventListener("keyup", handleKeyboardButtonPress);
 
 function continueGame() {
   const alphabet = getARandomAlphabet();
@@ -9,9 +38,7 @@ function continueGame() {
   setBackgroundColorById(alphabet);
 }
 
-
 function play() {
-
   // const homeScreen = document.getElementById('home-screen');
   // homeScreen.classList.add('hidden');
 
@@ -20,12 +47,4 @@ function play() {
   hideElementById("home-screen");
   showElementById("play-ground");
   continueGame();
-
 }
-
-
-
-
-
-    
-  
