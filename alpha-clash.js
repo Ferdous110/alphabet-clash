2b4143d5f4bd2c91e855de4b968bfc1c4f210274
ANJUM FERDOUS <anjumferdous110@gmail.com>
2024-11-23 13:49:34 +0600

    Change the background color of any specific key using random key

--- alpha-clash.js
+++ alpha-clash.js
@@ -2,7 +2,11 @@
 
 function continueGame() {
   const alphabet = getARandomAlphabet();
-  
+
+  const currentAlphabetElement = document.getElementById("current-alphabet");
+  currentAlphabetElement.innerText = alphabet;
+
+  setBackgroundColorById(alphabet);
 }
 
 
